3aa1391112041e3d4134f5426f818d8e60a21e0b
taufiqurrm <taufiqr719@gmail.com>
2023-05-30 21:25:21 +0700

    E-Surat

---
+++ README.md
@@ -0,0 +1,10 @@
+# Responsive Website
+
+<!-- ## [Watch it on youtube](link: ) -->
+
+### Responsive Web E-SURAT MTs. Bustanul Ulum
+
+Responsive Website Using Html, Css and CI
+
+
+![Preview](/preview.png)
---
+++ application/views/web/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<div class="footer text-muted text-center">
+	&copy; 2023. <a href="" class="text-success"><b>E-SURAT</b></a>
+</div>
+<!-- /footer -->
+</div>
+<!-- /content area -->
+</div>
+<!-- /main content -->
+</div>
+<!-- /page content -->
+</div>
+<!-- /page container -->
+</body>
+
+</html>
